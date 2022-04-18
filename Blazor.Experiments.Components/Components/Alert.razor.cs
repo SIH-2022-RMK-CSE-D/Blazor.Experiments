f37911e8dcf4f4fc169dc1871f4c23419f6ee196
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-04-18 19:39:58 +0530

    Added new component : Alert, and added example to the SurveyPrompt component

---
+++ Blazor.Experiments.Components/Components/Alert.razor.cs
@@ -0,0 +1,38 @@
+﻿namespace Blazor.Experiments.Components;
+
+public partial class Alert
+{
+    protected string ClassName => new CssBuilder().AddClass("alert")
+                                                  .AddClass($"alert-{Color.ToDescriptionString()}")
+                                                  .AddClass("alert-dismissible", Closable)
+                                                  .AddClass("alert-important", Important)
+                                                  .AddClass(Class)
+                                                  .ToString();
+
+    [Parameter]
+    public Color Color { get; set; }
+
+    [Parameter]
+    public bool NoIcon { get; set; }
+
+    [Parameter]
+    public string Icon { get; set; }
+
+    [Parameter]
+    public bool Important { get; set; }
+
+    [Parameter]
+    public bool Closable { get; set; }
+
+    [Parameter]
+    public string Title { get; set; }
+    
+    [Parameter]
+    public string Message { get; set; }
+
+    [Parameter]
+    public RenderFragment MessageContent { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+}
