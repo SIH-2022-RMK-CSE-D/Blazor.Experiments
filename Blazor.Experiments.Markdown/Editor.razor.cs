74235446ac1bba4fc6f9d8b270774372f50f2dc8
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-04-19 10:30:50 +0530

    Refactored Editor component into two files and added minor improvements

---
+++ Blazor.Experiments.Markdown/Editor.razor.cs
@@ -0,0 +1,55 @@
+﻿using Blazor.Experiments.Components.Helpers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace Blazor.Experiments.Markdown;
+
+public partial class Editor
+{
+    private FieldIdentifier _fieldIdentifier;
+    private string _fieldCssClasses => CascadedEditContext?.FieldCssClass(_fieldIdentifier) ?? "";
+
+    protected string PreviewClassName => new CssBuilder().AddClass("container").AddClass(PreviewClass).AddClass("p-4").Build();
+
+    [CascadingParameter]
+    private EditContext CascadedEditContext { get; set; }
+
+    [Parameter]
+    public string TextAreaClass { get; set; }
+
+    [Parameter]
+    public string TextAreaStyles { get; set; }
+
+    [Parameter]
+    public string PreviewClass { get; set; }
+
+    [Parameter]
+    public string PreviewStyles { get; set; }
+
+    [Parameter]
+    public string Value { get; set; }
+
+    [Parameter]
+    public bool ShowPreview { get; set; } = true;
+
+    [Parameter]
+    public EventCallback<string> ValueChanged { get; set; }
+
+    [Parameter]
+    public Expression<Func<string>> ValueExpression { get; set; }
+
+    [Parameter]
+    public RenderFragment ToolbarContent { get; set; }
+
+    protected override void OnInitialized()
+    {
+        _fieldIdentifier = FieldIdentifier.Create(ValueExpression);
+    }
+
+    public async Task HandleInput(ChangeEventArgs args)
+    {
+        await ValueChanged.InvokeAsync(args.Value.ToString());
+        CascadedEditContext?.NotifyFieldChanged(_fieldIdentifier);
+    }
+}
